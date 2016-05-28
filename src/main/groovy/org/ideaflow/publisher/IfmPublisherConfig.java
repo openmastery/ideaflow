d85699368891ef7bdc26591bd15aac3638285b3d
Mike Lueders <mike@roadkill.org>
2016-05-28 11:36:57 -0500

    add component scan for base package

--- src/main/groovy/org/ideaflow/publisher/IfmPublisherConfig.java
+++ src/main/groovy/org/ideaflow/publisher/IfmPublisherConfig.java
@@ -2,13 +2,17 @@ package org.ideaflow.publisher;
 
 import org.ideaflow.publisher.core.ideaflow.IdeaFlowInMemoryPersistenceService;
 import org.ideaflow.publisher.core.ideaflow.IdeaFlowPersistenceService;
+import org.openmastery.time.LocalDateTimeService;
+import org.openmastery.time.TimeService;
 import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.ideaflow.publisher")
 @EnableAutoConfiguration(exclude = {
 		SecurityAutoConfiguration.class,
 		ManagementWebSecurityAutoConfiguration.class})
@@ -17,6 +21,11 @@ public class IfmPublisherConfig {
 	@Bean
 	public IdeaFlowPersistenceService ideaFlowPersistenceService() {
 		return new IdeaFlowInMemoryPersistenceService();
+	}
+
+	@Bean
+	public TimeService timeService() {
+		return new LocalDateTimeService();
 	}
 
 }
