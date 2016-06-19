2f1c3d3467bbe1b46aabe092fb10a9d377bb7d42
janelle <janelle@newiron.com>
2016-06-18 20:23:31 -0500

    Updated idle_band constraints to not allow null taskId

--- src/main/resources/db/create_idle_time_band.sql
+++ src/main/resources/db/create_idle_time_band.sql
@@ -6,7 +6,7 @@ create sequence idle_time_band_seq
 --changeset ideaflow:4
 create table idle_time_band (
   id bigint constraint idle_time_band_pk primary key,
-  task_id bigint,
+  task_id bigint not null,
   start_time timestamp without time zone,
   end_time timestamp without time zone,
   comment varchar(250),
