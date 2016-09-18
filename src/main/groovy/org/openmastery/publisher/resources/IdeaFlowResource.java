038ca3d83f47b9b4e4b806596a3d2ffa9a84351f
Mike Lueders <mike@roadkill.org>
2016-09-18 15:43:27 -0500

    updated gradle-core to pull in latest license plugin; ran license plugin

--- src/main/groovy/org/openmastery/publisher/resources/IdeaFlowResource.java
+++ src/main/groovy/org/openmastery/publisher/resources/IdeaFlowResource.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2015 New Iron Group, Inc.
- * <p>
+ * Copyright 2016 New Iron Group, Inc.
+ *
  * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.gnu.org/licenses/gpl-3.0.en.html
- * <p>
+ *
+ * 	http://www.gnu.org/licenses/gpl-3.0.en.html
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
