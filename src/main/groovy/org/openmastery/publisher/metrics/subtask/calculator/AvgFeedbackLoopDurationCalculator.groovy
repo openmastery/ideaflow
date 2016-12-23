0b772038cbcdf532f08ba7bef7a2e7c1c2d13695
janelle <janelle@newiron.com>
2016-12-23 17:13:07 -0600

    Implement feedback loop duration calculator (duration/execCount)

--- src/main/groovy/org/openmastery/publisher/metrics/subtask/calculator/AvgFeedbackLoopDurationCalculator.groovy
+++ src/main/groovy/org/openmastery/publisher/metrics/subtask/calculator/AvgFeedbackLoopDurationCalculator.groovy
@@ -16,14 +16,124 @@
 package org.openmastery.publisher.metrics.subtask.calculator
 
 import org.joda.time.Duration
+import org.openmastery.publisher.api.event.ExecutionEvent
+import org.openmastery.publisher.api.ideaflow.IdeaFlowBand
+import org.openmastery.publisher.api.ideaflow.IdeaFlowStateType
 import org.openmastery.publisher.api.ideaflow.IdeaFlowTimeline
 import org.openmastery.publisher.api.metrics.Metric
+import org.openmastery.publisher.api.metrics.MetricType
 import org.openmastery.publisher.metrics.subtask.MetricsCalculator
 
 class AvgFeedbackLoopDurationCalculator implements MetricsCalculator<Duration> {
 
+
+	/**
+	 *
+	 * What's the ratio of troubleshooting time to execution events within a troubleshooting band?
+	 *
+	 * @param timeline for a subtask
+	 * @return Metric<Double> the resulting metric value
+	 */
 	@Override
 	Metric<Duration> calculateMetrics(IdeaFlowTimeline timeline) {
-		return null
+
+		List<IdeaFlowBand> troubleshootingBands = timeline.ideaFlowBands.findAll() { IdeaFlowBand band ->
+			band.type == IdeaFlowStateType.TROUBLESHOOTING
+		}
+
+		Double avgDuration = 0;
+		Long sampleCount = 0;
+
+		troubleshootingBands.each { IdeaFlowBand troubleshootingBand ->
+			Long relativeStart = troubleshootingBand.relativePositionInSeconds
+			Long relativeEnd = troubleshootingBand.relativePositionInSeconds + troubleshootingBand.durationInSeconds
+
+			int eventCount = countExecutionEventsInRange(timeline.executionEvents, relativeStart, relativeEnd)
+
+			Double durationRatio = ((double) troubleshootingBand.durationInSeconds) / eventCount
+			println durationRatio
+			sampleCount++
+			avgDuration = (avgDuration *(sampleCount - 1) + durationRatio)/sampleCount
+			println avgDuration
+
+		}
+
+		Metric<Duration> metric = new Metric<Duration>()
+		metric.type = MetricType.AVG_FEEDBACK_LOOP_DURATION //TODO refactor this stuff out to a property
+		metric.value = new Duration((long)(avgDuration * 1000))
+		return metric
+	}
+
+	int countExecutionEventsInRange(List<ExecutionEvent> executionEvents, Long relativeStart, Long relativeEnd ) {
+		println relativeStart + " : " + relativeEnd + " = " + (relativeEnd - relativeStart)
+		List<ExecutionEvent> eventsWithinRange = executionEvents.findAll() { ExecutionEvent event ->
+			event.relativePositionInSeconds > relativeStart &&
+					event.relativePositionInSeconds < relativeEnd
+		}
+		println "Result : "+eventsWithinRange.collect { it.relativePositionInSeconds }
+		return eventsWithinRange.size()
+	}
+
+	List<IdeaFlowBand> collapseConsecutiveBandPeriods(List<IdeaFlowBand> bands) {
+		List<IdeaFlowBand> filteredBands = bands.findAll() { IdeaFlowBand band ->
+			(band.type == IdeaFlowStateType.PROGRESS || band.type == IdeaFlowStateType.TROUBLESHOOTING)
+		}
+
+		filteredBands = filteredBands.sort { IdeaFlowBand band ->
+			band.relativePositionInSeconds
+		}
+
+		List<IdeaFlowBand> consecutiveBandPeriods = []
+		IdeaFlowBand lastBand = null
+		filteredBands.each { IdeaFlowBand band ->
+			if (lastBand == null) {
+				lastBand = band
+			} else {
+				if (bandsAreAdjacent(lastBand, band)) {
+					IdeaFlowBand newBand = IdeaFlowBand.builder()
+							.relativePositionInSeconds(lastBand.relativePositionInSeconds)
+							.durationInSeconds(lastBand.durationInSeconds + band.durationInSeconds)
+							.build()
+					lastBand = newBand
+				} else {
+					consecutiveBandPeriods.add(lastBand)
+					lastBand = band
+				}
+			}
+		}
+		if (lastBand) consecutiveBandPeriods.add(lastBand)
+
+		return consecutiveBandPeriods
+	}
+
+	boolean bandsAreAdjacent(IdeaFlowBand prevBand, IdeaFlowBand nextBand) {
+		Long prevBandEnd = prevBand.relativePositionInSeconds + prevBand.durationInSeconds
+		return (prevBandEnd == nextBand.relativePositionInSeconds)
+	}
+
+
+	List<Long> findRelativePositionsWithinRange(List<ExecutionEvent> allEvents, Long relativeStart, Long relativeEnd) {
+		List<ExecutionEvent> eventsWithinBand = findEventsWithinRange(allEvents, relativeStart, relativeEnd)
+		List<Long> relativeTimes = createRelativePositionsList(eventsWithinBand)
+		relativeTimes.add(relativeStart)
+		relativeTimes.add(relativeEnd)
+
+		Collections.sort(relativeTimes)
+		return relativeTimes
+	}
+
+	List<Long> createRelativePositionsList(List<ExecutionEvent> events) {
+		List<Long> relativeTimes = events.collect { ExecutionEvent event ->
+			event.relativePositionInSeconds
+		}
+		Collections.sort(relativeTimes)
+		return relativeTimes
+	}
+
+
+	List<ExecutionEvent> findEventsWithinRange(List<ExecutionEvent> events, Long relativeStart, Long relativeEnd) {
+		return events.findAll() { ExecutionEvent event ->
+			event.relativePositionInSeconds > relativeStart && event.relativePositionInSeconds < relativeEnd
+		}
 	}
 }
---
+++ src/test/groovy/org/openmastery/publisher/metrics/subtask/calculator/AvgFeedbackLoopDurationCalculatorSpec.groovy
@@ -0,0 +1,86 @@
+package org.openmastery.publisher.metrics.subtask.calculator
+
+import org.joda.time.Duration
+import org.joda.time.LocalDateTime
+import org.openmastery.publisher.api.ideaflow.IdeaFlowBand
+import org.openmastery.publisher.api.ideaflow.IdeaFlowStateType
+import org.openmastery.publisher.api.ideaflow.IdeaFlowTimeline
+import org.openmastery.publisher.api.metrics.Metric
+import org.openmastery.publisher.api.metrics.MetricType
+import org.openmastery.publisher.ideaflow.timeline.IdeaFlowTimelineElementBuilder
+import org.openmastery.time.MockTimeService
+import spock.lang.Specification
+
+
+class AvgFeedbackLoopDurationCalculatorSpec extends Specification {
+
+	private MockTimeService mockTimeService = new MockTimeService()
+	private IdeaFlowTimelineElementBuilder builder = new IdeaFlowTimelineElementBuilder(mockTimeService)
+
+	private AvgFeedbackLoopDurationCalculator calculator = new AvgFeedbackLoopDurationCalculator()
+
+
+	LocalDateTime start
+
+	def setup() {
+		start = mockTimeService.now()
+	}
+
+	def "calculateMetrics SHOULD return the ratio of (total time / numberEvents) "() {
+		given:
+		IdeaFlowBand troubleshootingBand = IdeaFlowBand.builder()
+				.type(IdeaFlowStateType.TROUBLESHOOTING)
+				.relativePositionInSeconds(0)
+				.durationInSeconds(15 * 60)
+				.build()
+
+		builder.activate()
+		builder.advanceMinutes(1)
+		builder.executeCode()
+		builder.advanceMinutes(1)
+		builder.executeCode()
+		builder.advanceMinutes(1)
+		builder.executeCode()
+
+		when:
+		IdeaFlowTimeline timeline = new IdeaFlowTimeline(ideaFlowBands: [troubleshootingBand], executionEvents: builder.executionEventList)
+		Metric<Duration> metric = calculator.calculateMetrics(timeline)
+
+		then:
+		assert metric.type == MetricType.AVG_FEEDBACK_LOOP_DURATION
+		assert metric.value == Duration.standardSeconds( (Long)(15 * 60)/ 3)
+	}
+
+	def "calculateMetrics SHOULD return the average of ratios WHEN there's multiple bands"() {
+		given:
+		IdeaFlowBand troubleshootingBand = IdeaFlowBand.builder()
+				.type(IdeaFlowStateType.TROUBLESHOOTING)
+				.relativePositionInSeconds(0)
+				.durationInSeconds(15 * 60)
+				.build()
+
+		IdeaFlowBand troubleshootingBand2 = IdeaFlowBand.builder()
+				.type(IdeaFlowStateType.TROUBLESHOOTING)
+				.relativePositionInSeconds(15 * 60)
+				.durationInSeconds(20 * 60)
+				.build()
+
+		builder.activate()
+		builder.advanceMinutes(5)
+		builder.executeCode()
+		builder.advanceMinutes(15) //5 min into the second band
+		builder.executeCode()
+		builder.advanceMinutes(1)
+		builder.executeCode()
+		builder.advanceMinutes(1)
+		builder.executeCode()
+
+		when:
+		IdeaFlowTimeline timeline = new IdeaFlowTimeline(ideaFlowBands: [troubleshootingBand, troubleshootingBand2], executionEvents: builder.executionEventList)
+		Metric<Duration> metric = calculator.calculateMetrics(timeline)
+
+		then:
+		assert metric.type == MetricType.AVG_FEEDBACK_LOOP_DURATION
+		assert metric.value == Duration.standardSeconds( (Long) ((15 * 60)/1 + (20 * 60)/3)/2 )
+	}
+}
