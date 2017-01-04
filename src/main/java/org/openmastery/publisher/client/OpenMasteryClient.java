2523047ea2fa3f87a30e4890567a04c176ed8e64
Mike Lueders <Mike.Lueders@blackbaud.com>
2017-01-04 08:43:50 -0600

    add read and connect timeout properties to OpenMasteryClient

--- src/main/java/org/openmastery/publisher/client/OpenMasteryClient.java
+++ src/main/java/org/openmastery/publisher/client/OpenMasteryClient.java
@@ -3,6 +3,7 @@ package org.openmastery.publisher.client;
 import com.bancvue.rest.client.ClientRequestFactory;
 import com.bancvue.rest.client.crud.CrudClient;
 import com.bancvue.rest.client.crud.CrudClientRequest;
+import org.glassfish.jersey.client.ClientProperties;
 import org.openmastery.publisher.api.ResourcePaths;
 
 public abstract class OpenMasteryClient<API_TYPE, CLIENT_TYPE extends CrudClient> extends CrudClient<API_TYPE, CLIENT_TYPE> {
@@ -21,6 +22,14 @@ public abstract class OpenMasteryClient<API_TYPE, CLIENT_TYPE extends CrudClient
 
 	public CLIENT_TYPE apiKey(String apiKey) {
 		return header(ResourcePaths.API_KEY_HEADER, apiKey);
+	}
+
+	public CLIENT_TYPE readTimeout(int readTimeoutMillis) {
+		return property(ClientProperties.READ_TIMEOUT, Integer.toString(readTimeoutMillis));
+	}
+
+	public CLIENT_TYPE connectTimeout(int connectTimeoutMillis) {
+		return property(ClientProperties.CONNECT_TIMEOUT, Integer.toString(connectTimeoutMillis));
 	}
 
 }
