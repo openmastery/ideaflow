069780673fdbb5f5a918198407c0e079159e5425
janelle <janelle@openmastery.org>
2017-01-13 09:55:21 -0600

    Create temporary workaround for TaskClient to support generic Pageable type

--- src/main/java/org/openmastery/publisher/client/TaskClient.java
+++ src/main/java/org/openmastery/publisher/client/TaskClient.java
@@ -1,9 +1,15 @@
 package org.openmastery.publisher.client;
 
+import com.bancvue.rest.client.crud.CrudClientRequest;
+import com.bancvue.rest.client.crud.GenericTypeFactory;
 import org.openmastery.publisher.api.ResourcePaths;
 import org.openmastery.publisher.api.task.NewTask;
 import org.openmastery.publisher.api.task.Task;
 import org.openmastery.publisher.api.ResourcePage;
+
+import javax.ws.rs.core.GenericType;
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class TaskClient extends IdeaFlowClient<Object, TaskClient> {
 
@@ -25,11 +31,23 @@ public class TaskClient extends IdeaFlowClient<Object, TaskClient> {
 				.path(taskName).entity(Task.class).find();
 	}
 
+	private static final GenericTypeFactory GENERIC_TYPE_FACTORY = GenericTypeFactory.getInstance();
+
 	public ResourcePage<Task> findRecentTasks(Integer page, Integer perPage) {
-        return (ResourcePage<Task>) getUntypedCrudClientRequest()
+		GenericType<ResourcePage<Task>> entityType = GENERIC_TYPE_FACTORY.createGenericType(ResourcePage.class, Task.class);
+		CrudClientRequest request = getUntypedCrudClientRequest()
                 .queryParam("page", page)
-                .queryParam("per_page", perPage)
-                .entity(ResourcePage.class).find();
+                .queryParam("per_page", perPage);
+
+		try {
+			Field entityField = request.getClass().getDeclaredField("entity");
+			entityField.setAccessible(true);
+			entityField.set(request, entityType);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+
+		return (ResourcePage<Task>) request.find();
 	}
 
 }
